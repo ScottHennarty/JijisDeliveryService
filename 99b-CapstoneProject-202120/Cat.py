971227ebaa597bf8055ac768e7a400a6029fe38b
hennarsp <hennarsp@rose-hulman.edi>
2021-02-10 15:51:55 -0500

    Almost nailed the walking animations for the dog and cat

--- 99b-CapstoneProject-202120/Cat.py
+++ 99b-CapstoneProject-202120/Cat.py
@@ -3,14 +3,83 @@ import sys
 import time
 import math
 
+
 class Cat:
     def __init__(self, screen):
         """ The cat sets the speed and is place in the center of the screen """
         self.screen = screen
         self.x = self.screen.get_width() // 2 - 50
         self.y = self.screen.get_height() // 2 - 50
-        self.standing_image = pygame.image.load("Cute_Kitten.png")
+        self.standing_left_1 = pygame.image.load("New Gato/Cat_Left_Idle_1.png")
+        self.standing_left_1_scaled = pygame.transform.scale(self.standing_left_1, (65, 60))
+        self.standing_right_1 = pygame.image.load("New Gato/Cat_Right_Idle_1.png")
+        self.standing_right_1_scaled = pygame.transform.scale(self.standing_right_1, (65, 60))
+        self.left_run_1 = pygame.image.load("New Gato/Cat_Left_Walk_2.png")
+        self.left_run_1_scaled = pygame.transform.scale(self.left_run_1, (65, 60))
+        self.left_run_2 = pygame.image.load("New Gato/Cat_Left_Walk_2.png")
+        self.left_run_2_scaled = pygame.transform.scale(self.left_run_2, (65, 60))
+        self.right_run_1 = pygame.image.load("New Gato/Cat_Right_Walk_1.png")
+        self.right_run_1_scaled = pygame.transform.scale(self.right_run_1, (65, 60))
+        self.right_run_2 = pygame.image.load("New Gato/Cat_Right_Walk_2.png")
+        self.right_run_2_scaled = pygame.transform.scale(self.right_run_2, (65, 60))
         self.speed = 3
+        self.last_pressed = True
+        self.time = 0
+        self.time_delayer = 0
+        self.one_or_two = 0
 
     def draw(self):
-        self.screen.blit(self.standing_image, (self.x, self.y))
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_RIGHT]:
+            self.walk_right()
+            return
+        if pressed_keys[pygame.K_LEFT]:
+            self.walk_left()
+            return
+        if pressed_keys[pygame.K_UP]:
+            if self.last_pressed == True:
+                self.walk_right()
+                return
+            else:
+                self.walk_left()
+                return
+        if pressed_keys[pygame.K_DOWN]:
+            if self.last_pressed == True:
+                self.walk_right()
+                return
+            else:
+                self.walk_left()
+                return
+        else:
+            if self.last_pressed == True:
+                self.screen.blit(self.standing_right_1_scaled, (self.x, self.y))
+            else:
+                self.screen.blit(self.standing_left_1_scaled, (self.x, self.y))
+
+    def walk_right(self):
+        if self.time_delayer % 5 == 0:
+            self.time = self.time + 1
+        if self.time % 2 == 0:
+            if self.one_or_two % 2 == 0:
+                self.screen.blit(self.right_run_2_scaled, (self.x, self.y))
+            else:
+                self.screen.blit(self.right_run_1_scaled, (self.x, self.y))
+        else:
+            self.screen.blit(self.standing_right_1_scaled, (self.x, self.y))
+            self.one_or_two = self.one_or_two + 1
+        self.time_delayer = self.time_delayer + 1
+        self.last_pressed = True
+
+    def walk_left(self):
+        if self.time_delayer % 5 == 0:
+            self.time = self.time + 1
+        if self.time % 2 == 0:
+            if self.one_or_two % 2 == 0:
+                self.screen.blit(self.left_run_2_scaled, (self.x, self.y))
+            else:
+                self.screen.blit(self.left_run_1_scaled, (self.x, self.y))
+        else:
+            self.screen.blit(self.standing_left_1_scaled, (self.x, self.y))
+            self.one_or_two = self.one_or_two + 1
+        self.time_delayer = self.time_delayer + 1
+        self.last_pressed = False
--- 99b-CapstoneProject-202120/Dog.py
+++ 99b-CapstoneProject-202120/Dog.py
@@ -9,80 +9,76 @@ class Dog:
         self.screen = screen
         self.x = x
         self.y = y
-        self.standing_right_1 = pygame.image.load("El Perro/Dog_Right_Idle_1.png")
-        self.standing_right_2 = pygame.image.load("El Perro/Dog_Right_Idle_2.png")
-        self.standing_left_1 = pygame.image.load("El Perro/Dog_Left_Idle_1.png")
-        self.standing_left_2 = pygame.image.load("El Perro/Dog_Left_Idle_2.png")
-        self.right_run_1 = pygame.image.load("El Perro/Dog_Right_Run_1.png")
-        self.right_run_2 = pygame.image.load("El Perro/Dog_Right_Run_2.png")
-        self.right_run_3 = pygame.image.load("El Perro/Dog_Right_Run_3.png")
-        self.left_run_1 =  pygame.image.load("El Perro/Dog_Left_Run_1.png")
-        self.left_run_2 = pygame.image.load("El Perro/Dog_Left_Run_2.png")
-        self.left_run_3 = pygame.image.load("El Perro/Dog_Left_Run_3.png")
+        self.standing_left_1 = pygame.image.load("New Perro/Dog_Left_Idle_1.png")
+        self.standing_left_1_scaled = pygame.transform.scale(self.standing_left_1, (70, 65))
+        self.standing_right_1 = pygame.image.load("New Perro/Dog_Right_Idle_1.png")
+        self.standing_right_1_scaled = pygame.transform.scale(self.standing_right_1, (70, 65))
+        self.left_run_1 = pygame.image.load("New Perro/Dog_Left_Walk_1.png")
+        self.left_run_1_scaled = pygame.transform.scale(self.left_run_1, (70, 65))
+        self.left_run_2 = pygame.image.load("New Perro/Dog_Left_Walk_2.png")
+        self.left_run_2_scaled = pygame.transform.scale(self.left_run_2, (70, 65))
+        self.left_run_3 = pygame.image.load("New Perro/Dog_Left_Walk_3.png")
+        self.left_run_3_scaled = pygame.transform.scale(self.left_run_3, (70, 65))
+        self.right_run_1 = pygame.image.load("New Perro/Dog_Right_Walk_1.png")
+        self.right_run_1_scaled = pygame.transform.scale(self.right_run_1, (70, 65))
+        self.right_run_2 = pygame.image.load("New Perro/Dog_Right_Walk_2.png")
+        self.right_run_2_scaled = pygame.transform.scale(self.right_run_2, (70, 65))
+        self.right_run_3 = pygame.image.load("New Perro/Dog_Right_Walk_3.png")
+        self.right_run_3_scaled = pygame.transform.scale(self.right_run_3, (70, 65))
         self.time = 0
         self.time_delayer = 0
         self.one_or_two = 0
         self.radius = 400
         self.screen_width = screen.get_width()
         self.screen_height = screen.get_height()
+        self.speed = 3
 
     def draw(self):
         if self.magnitude <= self.radius:
-            if (self.screen_width // 2) - self.x > 0:
-                if self.time_delayer % 9 == 0:
+            if (self.screen_width // 2) - self.x - 35 > 15:
+                if self.time_delayer % 11 == 0:
                     self.time = self.time + 1
                 if self.time % 2 == 0:
                     if self.one_or_two % 2 == 0:
-                        self.screen.blit(self.right_run_3, (self.x, self.y))
+                        self.screen.blit(self.right_run_3_scaled, (self.x, self.y))
                     else:
-                        self.screen.blit(self.right_run_1, (self.x, self.y))
+                        self.screen.blit(self.right_run_1_scaled, (self.x, self.y))
                 else:
-                    self.screen.blit(self.right_run_2, (self.x, self.y))
+                    self.screen.blit(self.right_run_2_scaled, (self.x, self.y))
                     self.one_or_two = self.one_or_two + 1
                 self.time_delayer = self.time_delayer + 1
-            elif(self.screen_width // 2) - self.x <= 0:
-                if self.time_delayer % 9 == 0:
+            elif(self.screen_width // 2) - self.x - 35 < -15:
+                if self.time_delayer % 11 == 0:
                     self.time = self.time + 1
                 if self.time % 2 == 0:
                     if self.one_or_two % 2 == 0:
-                        self.screen.blit(self.left_run_3, (self.x, self.y))
+                        self.screen.blit(self.left_run_3_scaled, (self.x, self.y))
                     else:
-                        self.screen.blit(self.left_run_1, (self.x, self.y))
+                        self.screen.blit(self.left_run_1_scaled, (self.x, self.y))
                 else:
-                    self.screen.blit(self.left_run_2, (self.x, self.y))
+                    self.screen.blit(self.left_run_2_scaled, (self.x, self.y))
                     self.one_or_two = self.one_or_two + 1
                 self.time_delayer = self.time_delayer + 1
+            #Here I will be adding up and down animations in an else loop but I need to do other work right now
         else:
             if (self.screen_width // 2) - self.x > 0:
-                if self.time_delayer % 13 == 0:
-                    self.time = self.time + 1
-                if self.time % 2:
-                    self.screen.blit(self.standing_right_1, (self.x, self.y))
-                else:
-                    self.screen.blit(self.standing_right_2, (self.x + 4, self.y + 2))
-                self.time_delayer = self.time_delayer + 1
+                    self.screen.blit(self.standing_right_1_scaled, (self.x, self.y))
             else:
-                if self.time_delayer % 13 == 0:
-                    self.time = self.time + 1
-                if self.time % 2:
-                    self.screen.blit(self.standing_left_1, (self.x, self.y))
-                else:
-                    self.screen.blit(self.standing_left_2, (self.x + 2, self.y + 2))
-                self.time_delayer = self.time_delayer + 1
+                    self.screen.blit(self.standing_left_1_scaled, (self.x, self.y))
 
 
 
 
     def move(self):
-        distance_x = (self.screen_width // 2) - self.x
-        distance_y = (self.screen_height // 2) - self.y
+        distance_x = (self.screen_width // 2) - self.x - 45
+        distance_y = (self.screen_height // 2) - self.y - 45
         self.magnitude = math.sqrt(distance_x ** 2 + distance_y ** 2)
         if self.magnitude <= self.radius:
-            if distance_x > 0:
-                self.x = self.x + 2
-            if distance_x < 0:
-                self.x = self.x - 2
-            if distance_y > 0:
-                self.y = self.y + 2
-            if distance_y < 0:
-                self.y = self.y - 2
+            if distance_x > -1:
+                self.x = self.x + self.speed
+            if distance_x < 1:
+                self.x = self.x - self.speed
+            if distance_y > -1:
+                self.y = self.y + self.speed
+            if distance_y < 1:
+                self.y = self.y - self.speed
