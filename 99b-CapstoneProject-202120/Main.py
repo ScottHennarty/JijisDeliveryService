40f64726a5e2c1b394557fbed7acc0d7a79704c6
neha vinesh <vineshn@rose-hulman.edu>
2021-02-12 08:51:47 -0500

    new stuff pt 3

--- 99b-CapstoneProject-202120/Main.py
+++ 99b-CapstoneProject-202120/Main.py
@@ -11,14 +11,15 @@ def main():
     pygame.display.set_caption("Jiji's Delivery Service ")
     screen = pygame.display.set_mode((1000, 800))
     clock = pygame.time.Clock()
-    world = pygame.Surface((1000,1000))
+    world = pygame.Surface((1000, 1000))
 
     cat = Cat(world)
-    walls = [Walls(world, 200, 200, 100, 300, (88, 88, 88), cat.speed), Walls(world, 800, 200, 100, 200, (88, 88, 88), cat.speed)]
+    walls = [Walls(world, 200, 200, 100, 300, (88, 88, 88), cat.speed),
+             Walls(world, 800, 200, 100, 200, (88, 88, 88), cat.speed)]
 
     dog = Dog(world, 700, 300)
 
-    camera_pos = (0,0)
+    camera_pos = (0, 0)
 
     collision_offset = 5
 
@@ -29,8 +30,8 @@ def main():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        #The function here will set the cat speed depending on if the cat is
-        #hitting a wall
+        # The function here will set the cat speed depending on if the cat is
+        # hitting a wall
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_RIGHT]:
             cat.speed_right = cat.speed
@@ -60,36 +61,36 @@ def main():
             # if wall.y + wall.height > wall.screen_height // 2 - 50 and wall.x + wall.width > wall.screen_width // 2 - 50 and wall.x < wall.screen_width // 2 and wall.y < wall.screen_height // 2:
             #     #wall.y_speed_up = 0
             #     cat.speed_up = 0
-            if cat.y + cat.height > wall.y and cat.y + cat.height // 2 <= wall.y+wall.height:
+            if cat.y + cat.height > wall.y and cat.y + cat.height // 2 <= wall.y + wall.height:
                 if wall.x < cat.x + collision_offset and wall.x + wall.width >= cat.x - collision_offset:
-                    #wall.x_speed_right = 0
+                    # wall.x_speed_right = 0
                     cat.speed_left = 0
                 if wall.x + wall.width > cat.x - collision_offset and wall.x < cat.x + cat.width + collision_offset:
-                    #wall.x_speed_left = 0
+                    # wall.x_speed_left = 0
                     cat.speed_right = 0
 
             if cat.x + cat.width >= wall.x and cat.x <= wall.x + wall.width:
                 if cat.y + cat.height + collision_offset >= wall.y:
-                    #wall.y_speed_down = 0
+                    # wall.y_speed_down = 0
                     cat.speed_down = 0
                 if wall.y + wall.height >= cat.y - collision_offset:
-                    #wall.y_speed_up = 0
+                    # wall.y_speed_up = 0
                     cat.speed_up = 0
 
-            if dog.y + dog.height > wall.y and dog.y + dog.height // 2 <= wall.y+wall.height:
+            if dog.y + dog.height > wall.y and dog.y + dog.height // 2 <= wall.y + wall.height:
                 if wall.x < dog.x + collision_offset and wall.x + wall.width >= dog.x - collision_offset:
-                    #wall.x_speed_right = 0
+                    # wall.x_speed_right = 0
                     dog.speed_left = 0
                 if wall.x + wall.width > dog.x - collision_offset and wall.x < dog.x + dog.width + collision_offset:
-                    #wall.x_speed_left = 0
+                    # wall.x_speed_left = 0
                     dog.speed_right = 0
 
             if dog.x + dog.width >= wall.x and dog.x <= wall.x + wall.width:
                 if dog.y + dog.height + collision_offset >= wall.y:
-                    #wall.y_speed_down = 0
+                    # wall.y_speed_down = 0
                     dog.speed_down = 0
                 if wall.y + wall.height >= dog.y - collision_offset:
-                    #wall.y_speed_up = 0
+                    # wall.y_speed_up = 0
                     dog.speed_up = 0
             """This block should be for the Dog's movement"""
             wall.draw()
